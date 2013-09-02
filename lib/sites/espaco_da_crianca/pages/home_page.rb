7e8a1555e551336d0a20d529e3f77420ef4261b2
Rafael Lima <rlima@thoughtworks.com>
2013-09-02 18:26:40 -0300

    adding home page

--- lib/sites/espaco_da_crianca/pages/home_page.rb
+++ lib/sites/espaco_da_crianca/pages/home_page.rb
@@ -1,9 +1,8 @@
-class HomePage
-  
-  URL = { :isolation => "http://espaco-da-crianca.herokuapp.com/" }
-  
-  def initialize(browser)
-    @browser = browser
+require 'rubygems'
+require 'taza/page'
+
+module EspacoDaCrianca
+  class HomePage < ::Taza::Page
+
   end
-  
 end
