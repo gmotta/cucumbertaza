f0a38aa87d4fa0f2b26136e9863abafaf1f7aec5
Rafael Lima <rlima@thoughtworks.com>
2013-09-02 18:03:04 -0300

    adding step definition

--- features/step_definitions/home_page.rb
+++ features/step_definitions/home_page.rb
@@ -1,6 +1,3 @@
-
-
-
 Given(/^the visitor is on the home page$/) do                                                                                                             
   pending # express the regexp above with the code you wish you had                                                                                       
 end                                                                                                                                                       
@@ -11,4 +8,41 @@ end
                                                                                                                                                           
 Then(/^the page url should contain 'activities'$/) do                                                                                                     
   pending # express the regexp above with the code you wish you had                                                                                       
+end
+
+Then(/^a "(.*?)" banner is displayed$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the page should contain 'Conheça nossas atividades'$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^he click on facebook link$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the facebook page is displayed$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^he click on facebook "(.*?)" link$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^facebook page is liked$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^he clicks on the 'Patrocínio' link$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^all logos of the sponsors must be displayed$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^contact information is displayed on the footer \(Blog, Facebook, YouTube, e\-Mail, Phone\)$/) do
+    pending # express the regexp above with the code you wish you had
+end
 end
