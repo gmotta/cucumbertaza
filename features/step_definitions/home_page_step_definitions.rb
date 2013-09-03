353413f3b72f658f4eba8dca087104738190fde2
gmotta <gfcmotta@gmail.com>
2013-09-03 13:45:34 -0300

    implemented a steps for the first scenario

--- features/step_definitions/home_page_step_definitions.rb
+++ features/step_definitions/home_page_step_definitions.rb
@@ -14,11 +14,11 @@ Given(/^the visitor is on the home page$/) do
 end                                                                                                                                                     
                                                                                                                                                           
 When(/^he clicks on Activities$/) do                                                                                                                      
-  pending # express the regexp above with the code you wish you had                                                                                       
+  @site.browser.goto "http://espaco-da-crianca.herokuapp.com/#activities"                                                                                      
 end                                                                                                                                                       
                                                                                                                                                           
 Then(/^the page url should contain 'activities'$/) do                                                                                                     
-  pending # express the regexp above with the code you wish you had                                                                                       
+   (@site.browser.url.include? "#activities").should == true  
 end
 
 # Then(/^a "(.*?)" banner is displayed$/) do |arg1|
