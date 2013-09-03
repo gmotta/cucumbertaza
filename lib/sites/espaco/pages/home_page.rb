fd41d7c62e1ef15f0339b83d8041f4f37c56dce5
Rafael Lima <rlima@thoughtworks.com>
2013-09-03 16:44:25 -0300

    adding feature file for atividades

--- lib/sites/espaco/pages/home_page.rb
+++ lib/sites/espaco/pages/home_page.rb
@@ -4,9 +4,13 @@ require 'taza/page'
 module Espaco
   class HomePage < ::Taza::Page
 
-  	def initialize()
-  		@site.browser.goto "http://espaco-da-crianca.herokuapp.com/"
-  	end
+  	#def initialize()
+  	#	@site.browser.goto "http://espaco-da-crianca.herokuapp.com/"
+  	#end
+
+    element(:sub_heading) { |name| browser.h1(:text => name }
+
+
 
   end
 end
