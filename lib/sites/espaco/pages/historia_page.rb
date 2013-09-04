49c747c53a9907516f26ce420e9a58c59226506f
Diego Asfora <dasfora@thoughtworks.com>
2013-09-04 16:52:45 -0300

    add history

--- lib/sites/espaco/pages/historia_page.rb
+++ lib/sites/espaco/pages/historia_page.rb
@@ -1,10 +1,12 @@
+# encoding: utf-8
+
 require 'rubygems'
 require 'taza/page'
 
 module Espaco
   class HistoriaPage < ::Taza::Page
+    element(:historia_link)         { browser.link(:text => 'História') }
 
-    
 
   end
 end
